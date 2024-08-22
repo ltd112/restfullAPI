683ee523398b30c1472164c15d6febace13272c3
chiton112 <lethanhdat032755@gmail.com>
2024-08-22 10:26:22 +0700

    update repository

--- src/main/java/com/dat/restfullAPI/repository/UserRepository.java
+++ src/main/java/com/dat/restfullAPI/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.dat.restfullAPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    User findByUserId(Long userId);
+    List<User> findAll();
+
 }
